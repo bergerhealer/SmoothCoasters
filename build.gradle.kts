93daa132b2287679acaee5366a3e6f49a339ec13
56738 <mineplayer64@gmail.com>
2021-11-06 15:36:32 +0100

    Release 1.17.1-v2

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,13 +5,13 @@ plugins {
 }
 
 group = "me.m56738"
-version = "1.17.1-v2-SNAPSHOT"
+version = "1.17.1-v2"
 
 dependencies {
     minecraft("com.mojang:minecraft:1.17.1")
-    mappings("net.fabricmc:yarn:1.17.1+build.61:v2")
-    modImplementation("net.fabricmc:fabric-loader:0.12.2")
-    modImplementation(fabricApi.module("fabric-networking-api-v1", "0.40.8+1.17"))
+    mappings("net.fabricmc:yarn:1.17.1+build.63:v2")
+    modImplementation("net.fabricmc:fabric-loader:0.12.5")
+    modImplementation(fabricApi.module("fabric-networking-api-v1", "0.42.0+1.17"))
 }
 
 java {
