80787d07189fb8369c8cf39e9eaf147ba0a3a722
56738 <mineplayer64@gmail.com>
2021-12-29 11:39:01 +0100

    Update to 1.18.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,10 +8,10 @@ group = "me.m56738"
 version = "1.18-v1-SNAPSHOT"
 
 dependencies {
-    minecraft("com.mojang:minecraft:1.18")
-    mappings("net.fabricmc:yarn:1.18+build.1:v2")
-    modImplementation("net.fabricmc:fabric-loader:0.12.6")
-    modImplementation(fabricApi.module("fabric-networking-api-v1", "0.43.1+1.18"))
+    minecraft("com.mojang:minecraft:1.18.1")
+    mappings("net.fabricmc:yarn:1.18.1+build.12:v2")
+    modImplementation("net.fabricmc:fabric-loader:0.12.12")
+    modImplementation(fabricApi.module("fabric-networking-api-v1", "0.45.0+1.18"))
 }
 
 java {
