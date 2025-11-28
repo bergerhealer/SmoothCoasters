3bac3a4648878ed386c3db6996e1c33dd08dbb4b
56738 <git@56738.me>
2025-11-28 13:40:34 +0100

    Configure GitHub Actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,11 @@
+import net.darkhax.curseforgegradle.TaskPublishCurseForge
+
 plugins {
     id("java")
     id("maven-publish")
     alias(libs.plugins.fabric.loom)
+    alias(libs.plugins.minotaur)
+    alias(libs.plugins.curseforgegradle)
 }
 
 group = "me.m56738"
@@ -11,11 +15,13 @@ dependencies {
     minecraft(libs.minecraft)
     mappings(loom.officialMojangMappings())
     modImplementation(libs.fabric.loader)
-    modImplementation(include(fabricApi.module("fabric-key-binding-api-v1", libs.versions.fabric.api.get()))!!)
-    modImplementation(include(fabricApi.module("fabric-lifecycle-events-v1", libs.versions.fabric.api.get()))!!)
-    modImplementation(include(fabricApi.module("fabric-networking-api-v1", libs.versions.fabric.api.get()))!!)
-    modImplementation(include(fabricApi.module("fabric-transitive-access-wideners-v1", libs.versions.fabric.api.get()))!!)
-    modImplementation(include(fabricApi.module("fabric-api-base", libs.versions.fabric.api.get()))!!)
+
+    val fabricApiVersion = libs.versions.fabric.api.get()
+    modImplementation(include(fabricApi.module("fabric-key-binding-api-v1", fabricApiVersion))!!)
+    modImplementation(include(fabricApi.module("fabric-lifecycle-events-v1", fabricApiVersion))!!)
+    modImplementation(include(fabricApi.module("fabric-networking-api-v1", fabricApiVersion))!!)
+    modImplementation(include(fabricApi.module("fabric-transitive-access-wideners-v1", fabricApiVersion))!!)
+    modImplementation(include(fabricApi.module("fabric-api-base", fabricApiVersion))!!)
 }
 
 java {
@@ -31,5 +37,24 @@ tasks {
         filesMatching("fabric.mod.json") {
             expand("version" to project.version)
         }
+    }
+}
+
+modrinth {
+    projectId = "smoothcoasters"
+    uploadFile.set(tasks.remapJar)
+    changelog = provider { rootProject.file("CHANGELOG.md").readText() }
+    syncBodyFrom = provider { rootProject.file("README.md").readText() }
+}
+
+tasks {
+    register("publishCurseForge", TaskPublishCurseForge::class) {
+        apiToken = System.getenv("CURSEFORGE_TOKEN")
+        val mainFile = upload(397480, remapJar)
+        mainFile.changelog = rootProject.file("CHANGELOG.md").readText()
+        mainFile.changelogType = "markdown"
+        mainFile.releaseType = "release"
+        mainFile.addGameVersion(libs.versions.minecraft.get())
+        mainFile.addEnvironment("Client")
     }
 }
